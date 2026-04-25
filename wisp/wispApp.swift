ca8cce6a58a07f9dc08df5a0d2f231a845aed73d
barrydeen <barrydeen@protonmail.com>
2026-04-24 21:09:48 -0400

    Initial Commit

---
+++ wisp/wispApp.swift
@@ -0,0 +1,17 @@
+//
+//  wispApp.swift
+//  wisp
+//
+//  Created by Barry Deeb on 2026-04-24.
+//
+
+import SwiftUI
+
+@main
+struct wispApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wispTests/wispTests.swift
@@ -0,0 +1,18 @@
+//
+//  wispTests.swift
+//  wispTests
+//
+//  Created by Barry Deeb on 2026-04-24.
+//
+
+import Testing
+
+struct wispTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
